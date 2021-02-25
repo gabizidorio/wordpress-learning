98cc9703b61b44149c84f4c7eaa43855e03b2cd3
Gabriela Izidorio <gabrielaizidorio18@gmail.com>
2021-02-25 20:41:47 +0000

    Update cmb2 functions

--- testewp/app/public/wp-content/themes/rest/functions.php
+++ testewp/app/public/wp-content/themes/rest/functions.php
@@ -1,5 +1,15 @@
 <?php
+    add_action('cmb2_admin_init', 'cmb2_fields_home');
+
     function cmb2_fields_home() {
-        
+        new_cmb2_box([
+            'id' => 'home_box',
+            'title' => 'Home',
+            'object_types' => ['page'],
+            'show_on' => [
+                'key' => 'page-template',
+                'value' => 'page-home.php'
+            ],
+        ]);
     }
 ?>
